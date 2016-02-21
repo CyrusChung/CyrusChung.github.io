95550c2ac82ee54d6ebf34bd92bdd9c99e41a969
Yanfong Chung <Yanfong Chung>
2016-02-22 04:25:26 +0800

    add lightbox feature

---
+++ _posts/2016-02-22-lightbox-function-added.markdown
@@ -0,0 +1,8 @@
+---
+layout: archive
+title:  "Lightbox feature added"
+date:   2016-02-19 04:19:00 +0800
+---
+I have added lightbox feature to this site via jQuery, now can click the wechat icon in the footer and scan the QRcode to aad me.
+
+为图片加入了lightbox的功能，现在可以通过点击footer里面的微信图标，显示我的微信二维码了。
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 $('#menu-btn').click(function(){
 	$('#menu-content').toggleClass('expand')
-})
+});
+
+//lightbox function
+$('.light').click(function(img) {
+	img.preventDefault();
+	var imgSrc = $(this).attr("href");
+	var lightbox = 
+		'<div id="lightbox"><img src="' + imgSrc + '">' + '</div>';
+	$(lightbox).appendTo('#wrap');
+});
+
+$('body').on("click", '#lightbox', function() {
+	$('#lightbox').remove();
+});
